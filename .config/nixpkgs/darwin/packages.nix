8c3776271110fda8004e9cf459f6efa45bc8ccc4
Axel Neergaard <axel.neergaard@cujo.com>
2025-01-25 22:29:40 +0200

    update: darwin packages

--- .config/nixpkgs/darwin/packages.nix
+++ .config/nixpkgs/darwin/packages.nix
@@ -2,6 +2,7 @@ pkgs: with pkgs; [
   # General tools
   htop
   tmux
+  neovim
   vim
 
   # Terminal tools
@@ -17,14 +18,18 @@ pkgs: with pkgs; [
   s3cmd
 
   # Terminal theming
+  nerdfonts
   powerline
   starship
 
   # Development tools
   aws-vault
   awscli2
+  cargo
+  colima
   duckdb
   # localstack  # currently fails...
+  nodejs_23  # Latest NodeJS version (2025/01/25), `nodejs` points to `20.18.1`
   pqrs
   terragrunt
   tgswitch
