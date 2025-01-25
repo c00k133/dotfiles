d9d6863c6ce1f89fa6cf5b6a12bf35eefbefc486
Axel Neergaard <axel.neergaard@cujo.com>
2025-01-25 22:59:12 +0200

    feat(nvim): set Vim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,4 +2,22 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.mapleader = " "
+
+--- LazyVim settings
 vim.g.snacks_animate = false
+
+--- Indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- Highlight search text
+vim.opt.hlsearch = true
+
+-- Make tabs 4 spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Finding files
+vim.opt.path:append({ "**" })
