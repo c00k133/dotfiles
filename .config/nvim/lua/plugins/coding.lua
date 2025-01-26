4eed90937e7ccc2d961010126538b9fb10f12a03
Axel Neergaard <axel.neergaard@cujo.com>
2025-01-26 19:56:28 +0200

    config(nvim): add the refactoring plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -6,4 +6,22 @@ return {
     cmd = "IncRename",
     config = true,
   },
+
+  -- Refactoring tool
+  {
+    "ThePrimeagen/refactoring.nvim",
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        modee = "v",
+        noremap = true,
+        silent = true,
+        expr = false,
+      },
+    },
+    opts = {},
+  },
 }
